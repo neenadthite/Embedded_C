f3ad51c2569830a3549b94153ed6850eec680010
ninad.thite <ninad.thite>
2025-03-15 12:22:04 +0530

    created repo for embedded c projects and added source codes

---
+++ My_Workspace/host/000TestProject/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 14, 2025
+ *      Author: 141584
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World\n");
+
+	printf("Press enter to exit the application\n");
+
+	getchar();
+
+	return 0;
+}
